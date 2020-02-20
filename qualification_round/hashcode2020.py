5f238455aed3e11627243d6699b58c79fd5c0164
v4570 <vasilis.tos@gmail.com>
2020-02-20 21:06:30 +0200

    Solution 1

--- qualification_round/hashcode2020.py
+++ qualification_round/hashcode2020.py
@@ -1,5 +1,7 @@
+import numpy as np
+import sys
 
-file_name = 'a_example.txt'
+file_name = sys.argv[1]
 
 with open(file_name, 'r') as fin:
     B, L, D = fin.readline().split()
@@ -7,13 +9,16 @@ with open(file_name, 'r') as fin:
     L = int(L)
     D = int(D)
 
-    scores = [int(x) for x in fin.readline().split()]
 
+    scores = np.array([int(x) for x in fin.readline().split()])
 
+    sorted_scores_index = np.argsort(scores*(-1))
+
+    # [number of books, signup finish, no. can be shipped]
     library_data = {}
-    library_books = {}
-    i = 0
 
+    # books in library
+    library_books = {}
 
     for i in range(L):
         line = fin.readline()
@@ -25,3 +30,33 @@ with open(file_name, 'r') as fin:
         books = [int(x) for x in line.split()]
         library_books[i] = books
 
+current_day = 0
+output = {}
+
+
+for i in sorted_scores_index:
+    for library, list_books in library_books.items():
+        if i in list_books:
+            
+            # library: (max, [which])
+            # max = (D - current_day - signup_time)*per_day
+            if not library in output:
+                sign_up = library_data[library][1]
+                max_books = (D - current_day - sign_up) * library_data[library][2]
+                current_day = current_day + sign_up
+                books = []
+                books.append(i)
+                output[library] = [max_books-1, books]
+
+                break
+            elif output[library][0] > 0:
+                output[library][1].append(i)
+                output[library][0] = output[library][0] - 1
+            
+                break
+print(len(output))
+
+for library, value in output.items():
+
+    print(library, len(value[1]))
+    print(*value[1])
