324d67ff94370be5d3b2de6a3400195aaca67e1d
v4570 <vasilis.tos@gmail.com>
2020-02-21 05:18:25 +0200

    always like this

--- qualification_round/hashcode2020.py
+++ qualification_round/hashcode2020.py
@@ -15,7 +15,7 @@ with open(file_name, 'r') as fin:
 
     
     sorted_scores_index = np.argsort(scores*(-1))
-    print('End')
+    
 
 
     # [number of books, signup finish, no. can be shipped]
@@ -32,33 +32,77 @@ with open(file_name, 'r') as fin:
         line = fin.readline()
         
         books = [int(x) for x in line.split()]
-        library_books[i] = books
+
+
+        sorted_books = [x for _,x in sorted(zip(sorted_scores_index, books))]
+        library_books[i] = sorted_books
+
+
+libraries_times = np.array([x[1] for _, x in library_data.items()])
+libraries_sorted = np.argsort(libraries_times)
 
 current_day = 0
 output = {}
 
 
+# for i in sorted_scores_index:
+#     if current_day >= D:
+#         break
+#     print(current_day)
+#     for library in libraries_sorted:
+#         list_books = library_books[library]
+#         if np.isin(i, list_books):
+            
+#             # library: (max, [which])
+#             # max = (D - current_day - signup_time)*per_day
+#             if not library in output:
+#                 sign_up = library_data[library][1]
+#                 max_books = (D - current_day - sign_up) * library_data[library][2]
+#                 current_day = current_day + sign_up
+#                 books = []
+#                 books.append(i)
+#                 output[library] = [max_books-1, books]
+                
+#                 # sorted_scores_index.remove(i)
+                
+#                 break
+#             elif output[library][0] > 0:
+#                 output[library][1].append(i)
+#                 output[library][0] = output[library][0] - 1
+
+#                 # sorted_scores_index.remove(i)
+
+#                 break
+
 for i in sorted_scores_index:
-    for library, list_books in library_books.items():
-        if i in list_books:
-            
-            # library: (max, [which])
-            # max = (D - current_day - signup_time)*per_day
-            if not library in output:
-                sign_up = library_data[library][1]
-                max_books = (D - current_day - sign_up) * library_data[library][2]
-                current_day = current_day + sign_up
-                books = []
-                books.append(i)
-                output[library] = [max_books-1, books]
+    if current_day >= D:
+        break
+    print(current_day)
+    for library in libraries_sorted:
+        library_list = library_books[library]
+        #print(library_list)
+        if np.isin(i, library_list):
 
-                break
-            elif output[library][0] > 0:
-                output[library][1].append(i)
-                output[library][0] = output[library][0] - 1
+            sign_up = library_data[library][1]
+            max_books = (D - current_day - sign_up) * library_data[library][2]
+            print('Max books '+str(max_books)+' library '+ str(library) +  ' library length ' + str(len(library_list)))
             
-                break
+            current_day = current_day + sign_up
+            
+            index = library_list.index(i)
+            print('index ' + str(index))
+            
+            books = []
+            books.append(i)
+            output[library] = books
+            max_books = max_books - 1
 
+            for j in range(index+1, min((len(library_list) - index), max_books)):
+                output[library].append(library_list[j])
+                np.delete(sorted_scores_index, library_list[j])
+            
+            library_books[library] = []
+                
 with open(sys.argv[2], 'w') as fout:
 
     fout.write(str(len(output))+'\n')
@@ -66,10 +110,10 @@ with open(sys.argv[2], 'w') as fout:
     for library, value in output.items():
 
         fout.write(str(library)+' ')
-        fout.write(str(len(value[1])))
+        fout.write(str(len(value)))
         fout.write('\n')
         
-        for v in value[1]:
+        for v in value:
             fout.write(str(v)+' ')
 
         fout.write('\n')
---
+++ qualification_round/hashcode2020v2.py
@@ -0,0 +1,86 @@
+import sys
+import numpy as np
+
+file_name = sys.argv[1]
+
+with open(file_name, 'r') as fin:
+    
+    B, L, D = fin.readline().split()
+    B = int(B)
+    L = int(L)
+    D = int(D)
+
+    scores = np.array([int(x) for x in fin.readline().split()])
+
+    
+    sorted_scores_index = np.argsort(scores*(-1))
+    
+    # [number of books, signup finish, no. can be shipped]
+    library_data = []
+
+    # books in library
+    library_books = []
+    
+    for i in range(L):
+        line = fin.readline()
+        data = [int(x) for x in line.split()]
+
+        library_data.append(data)
+
+        line = fin.readline()
+        
+        books = [(scores[int(x)],int(x)) for x in line.split()]
+        
+        sorted_books = sorted(books, reverse=True)
+        
+
+        library_books.append([x[1] for x in sorted_books])
+
+signup_times = np.array([x[1] for x in library_data])
+sorted_libraries = np.argsort(signup_times)
+
+current_day = 0
+total_scanned = []
+output = {}
+
+for library in sorted_libraries:
+    print('Current day ',current_day, ' Library ', library)
+    if current_day >= D:
+        break
+    signup_time = library_data[library][1]
+
+    max_books = (D - current_day - signup_time)*library_data[library][2]
+    
+    current_day = current_day + signup_time
+    
+    scanned = []
+    for i in range(min(len(library_books[library])-1, max_books)):
+        scanned.append(library_books[library][i])
+
+    # i = 0
+    # while max_books >= 0 and i < len(sorted_scores_index):
+    #     curr_book = sorted_scores_index[i]
+        
+    #     if np.isin(curr_book, library_books[library]):
+    #         scanned.append(curr_book)
+    #         sorted_scores_index = np.delete(sorted_scores_index, i)
+    #         max_books = max_books - 1
+        
+    #     i = i + 1
+    if scanned:
+        output[library] = scanned
+
+with open(sys.argv[2], 'w') as fout:
+
+    fout.write(str(len(output))+'\n')
+
+    for library, value in output.items():
+
+        fout.write(str(library)+' ')
+        fout.write(str(len(value)))
+        fout.write('\n')
+        
+        for v in value:
+            fout.write(str(v)+' ')
+    
+        fout.write('\n')
